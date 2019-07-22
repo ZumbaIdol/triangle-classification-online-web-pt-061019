1f1fcd586473cdbffdf510ca1b3cc9ea84e4d2ed
David Foote <david.a.foote@gmail.com>
2019-07-22 01:59:01 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,7 +1,10 @@
 class Triangle
-
-  def initialize(side_one, side_two, side_three)
-    
+  attr_accessor :equilateral, :isosceles, :scalene
+  
+  def initialize(attributes)
+    attributes.each do |key, value| 
+      self.send(("#{key}="), value)
+    end
   end
   
   def kind
